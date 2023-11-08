a0f94683fb4175124ae56463b1c6ec84180b10bb
Ryan <stander.ryan@gmail.com>
2023-11-08 16:58:41 +0100

    Add scripts
    It works

---
+++ AfterlifeShiftGame/Source/AfterlifeShiftGame/Scale/Scale.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Scale.h"
+
+
+// Sets default values
+AScale::AScale()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AScale::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AScale::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
