89683581feb6a79af53724daebd9a2eab344de1e
Ryan <stander.ryan@gmail.com>
2023-11-21 13:14:34 +0100

    big topper man

--- AfterlifeShiftGame/Source/AfterlifeShiftGame/AfterlifeShiftGame.Build.cs
+++ AfterlifeShiftGame/Source/AfterlifeShiftGame/AfterlifeShiftGame.Build.cs
@@ -10,7 +10,7 @@ public class AfterlifeShiftGame : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "EnhancedInput", "AIModule", "Niagara" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
