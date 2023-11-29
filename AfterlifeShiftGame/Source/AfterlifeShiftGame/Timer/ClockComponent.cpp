a54969f3738a62a11ea626da59e16680f710ba1b
Ryan <stander.ryan@gmail.com>
2023-11-29 19:04:14 +0100

    basic timer code

---
+++ AfterlifeShiftGame/Source/AfterlifeShiftGame/Timer/ClockComponent.cpp
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2023, Stinky Cheese, All rights reserved.
+
+
+#include "ClockComponent.h"
+
+
+// Sets default values for this component's properties
+UClockComponent::UClockComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// Initialize variables
+	CurrentTimeInHours = 0.0f;
+	TimeMultiplier = 0.01666f;  // Set to the following value so that 1 minute=1 hour in game
+}
+
+
+// Called when the game starts
+void UClockComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UClockComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Update the current time based on the time multiplier
+	CurrentTimeInHours += DeltaTime * TimeMultiplier;
+
+	// Normalize the time to a 24-hour cycle
+	if (CurrentTimeInHours >= 24.0f)
+	{
+		CurrentTimeInHours = FMath::Fmod(CurrentTimeInHours, 24.0f);
+	}
+}
+
+// Set the time multiplier
+void UClockComponent::SetTimeMultiplier(float Multiplier)
+{
+	TimeMultiplier = FMath::Max(Multiplier, 0.1f);  // Ensure the multiplier is non-negative
+}
+
+// Get the current time
+float UClockComponent::GetCurrentTime() const
+{
+	return CurrentTimeInHours;
+}
+
---
+++ AfterlifeShiftGame/Source/AfterlifeShiftGame/Timer/ClockComponent.h
@@ -0,0 +1,41 @@
+﻿// Copyright (c) 2023, Stinky Cheese, All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ClockComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class AFTERLIFESHIFTGAME_API UClockComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UClockComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	//Time functionality
+	UFUNCTION(BlueprintCallable, Category="Clock")
+	void SetTimeMultiplier(float Multiplier);
+
+	UFUNCTION(BlueprintCallable, Category="Clock")
+	float GetCurrentTime() const;
+
+private:
+	UPROPERTY(EditAnywhere, Category="Clock")
+	float CurrentTimeInHours;
+
+	UPROPERTY(EditAnywhere, Category="Clock")
+	float TimeMultiplier;
+};
